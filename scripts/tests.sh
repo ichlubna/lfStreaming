97eb7b1ec99227006424886f4ac3bb69e6ae95f3
ichlubna <43234438+ichlubna@users.noreply.github.com>
2023-10-03 11:52:30 +0200

    Update tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -6,6 +6,7 @@ DATAPATH=./oneTimeFrame
 ./testKeyFramePosition.sh $DATAPATH/animeTimelapse 2.046 0.0_0.29 8 8 timelapse.csv timelapseV.csv 
 ./testKeyFramePosition.sh $DATAPATH/animKey 1.909 0.19_0.53 8 8 key.csv keyV.csv 
 
+DATAPATH=./videos
 ./testGoPSize.sh $DATAPATH/animBonfire 2.276 0.06_0.3 8 8 bonfireG.csv bonfireVG.csv 
 ./testGoPSize.sh $DATAPATH/animStreet 1.816 0.16_0.29 8 8 streetG.csv streetVG.csv 
 ./testGoPSize.sh $DATAPATH/animeTimelapse 2.046 0.0_0.29 8 8 timelapseG.csv timelapseVG.csv 
