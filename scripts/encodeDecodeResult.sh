4fe0718aec72516d468fdc7271fb7ebe4ae14ad1
ichlubna <43234438+ichlubna@users.noreply.github.com>
2023-11-04 10:01:15 +0100

    Update encodeDecodeResult.sh

--- scripts/encodeDecodeResult.sh
+++ scripts/encodeDecodeResult.sh
@@ -13,16 +13,22 @@ OUTPUT=$TEMP"/output"
 mkdir $OUTPUT
 FORMAT=libaom-av1
 
+OUT_ORIGINAL=$OUTDIR"/original"
+OUT_DECODED=$OUTDIR"/decoded"
+mkdir $OUT_ORIGINAL
+mkdir $OUT_DECODED
+
 FILENAME=$(basename -- "$REFERENCE")
 EXTENSION="${FILENAME##*.}"
 cp $DIR"/"$REFERENCE $INPUT"/01."$EXTENSION
 for file in $DIR/*
 do
     cp $file $INPUT"/02."$EXTENSION
+    cp $file $OUT_ORIGINAL
     $FFMPEG -i $INPUT"/%02d."$EXTENSION -crf $CRF -c:v $FORMAT -cpu-used 8 -row-mt 1 -tiles 2x2 $OUTPUT"/encoded.mkv"  
     $FFMPEG -i $OUTPUT"/encoded.mkv" $OUTPUT"/%02d."$EXTENSION
     TRIMFILE=$(basename -- "$file")
-    mv $OUTPUT"/02."$EXTENSION $OUTDIR"/"$TRIMFILE 
+    mv $OUTPUT"/02."$EXTENSION $OUT_DECODED"/"$TRIMFILE
     rm $INPUT"/02."$EXTENSION
     rm $OUTPUT"/encoded.mkv"
     rm $OUTPUT"/01."$EXTENSION
