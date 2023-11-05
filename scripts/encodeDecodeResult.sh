db6b0ca80ef7b8cc4ed89209d77a945968972055
ichlubna <43234438+ichlubna@users.noreply.github.com>
2023-11-05 19:19:29 +0100

    Update encodeDecodeResult.sh

--- scripts/encodeDecodeResult.sh
+++ scripts/encodeDecodeResult.sh
@@ -25,7 +25,7 @@ for file in $DIR/*
 do
     cp $file $INPUT"/02."$EXTENSION
     cp $file $OUT_ORIGINAL
-    $FFMPEG -i $INPUT"/%02d."$EXTENSION -pix_fmt yuv420p -crf $CRF -c:v $FORMAT -cpu-used 8 -row-mt 1 -tiles 2x2 $OUTPUT"/encoded.mkv"  
+    $FFMPEG -i $INPUT"/%02d."$EXTENSION -pix_fmt yuv420p -crf $CRF -c:v $FORMAT -g 10 -keyint_min 10 -cpu-used 8 -row-mt 1 -tiles 2x2 $OUTPUT"/encoded.mkv"  
     $FFMPEG -i $OUTPUT"/encoded.mkv" $OUTPUT"/%02d."$EXTENSION
     TRIMFILE=$(basename -- "$file")
     mv $OUTPUT"/02."$EXTENSION $OUT_DECODED"/"$TRIMFILE
@@ -33,7 +33,7 @@ do
     rm $OUTPUT"/encoded.mkv"
     rm $OUTPUT"/01."$EXTENSION
 done
-$FFMPEG -i $INPUT"/%02d."$EXTENSION -pix_fmt yuv420p -crf $CRF -c:v $FORMAT -cpu-used 8 -row-mt 1 -tiles 2x2 $OUTPUT"/encoded.mkv"  
+$FFMPEG -i $INPUT"/%02d."$EXTENSION -pix_fmt yuv420p -crf $CRF -c:v $FORMAT -g 10 -keyint_min 10 -cpu-used 8 -row-mt 1 -tiles 2x2 $OUTPUT"/encoded.mkv"  
 $FFMPEG -i $OUTPUT"/encoded.mkv" $OUTPUT"/%02d."$EXTENSION
 TRIMFILE=$(basename -- "$REFERENCE")
 mv $OUTPUT"/01."$EXTENSION $OUTDIR"/"$TRIMFILE 
